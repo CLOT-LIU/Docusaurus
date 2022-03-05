9a0d0888f66324dcfc99a64813bc90c82e6c9b2f
LIU <66453914+CLOT-LIU@users.noreply.github.com>
2022-03-06 04:35:22 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -105,7 +105,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} LIU, Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
